ca864c21fd8d80305008538785a3823caa663ae7
Szilágyi <zsoltika20030@gmail.com>
2022-11-14 13:54:12 +0100

    Feladat

---
+++ 3.8/7. Feladat.py
@@ -0,0 +1,15 @@
+xs=[160, -43, 270, -97, -43, 200, -940, 17, -86]
+import turtle
+kép=turtle.Screen()
+Kalóz=turtle.Turtle()
+for x in xs:
+    Kalóz.forward(100)
+    if x>0:
+        Kalóz.left(x)
+    else:
+        Kalóz.right(x)
+összeg=0
+for x in xs:
+    összeg=összeg+x
+print(f"Jelenleg {összeg} fokban áll a hajó")
+kép.mainloop()

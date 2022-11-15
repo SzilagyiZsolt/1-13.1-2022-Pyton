982dbdc2156d3364f357cee6f466894578f5c434
Szilágyi <zsoltika20030@gmail.com>
2022-11-15 14:19:23 +0100

    Feladat

---
+++ 4.9/5.1. Feladat.py
@@ -0,0 +1,13 @@
+import turtle
+kép=turtle.Screen()
+Sanyi=turtle.Turtle()
+kép.bgcolor("lightgreen")
+Sanyi.color("blue")
+def megy(t, h):
+    t.right(90)
+    t.forward(h)
+meret=1
+for x in range(100):
+    megy(Sanyi, meret)
+    meret=meret+1
+kép.mainloop()

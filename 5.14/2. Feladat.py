77d1ce9394835cb0748181bbc08bffaf68144c66
Szilágyi <zsoltika20030@gmail.com>
2022-11-21 20:41:02 +0100

    Feladat

--- 5.14/2. Feladat.py
+++ 5.14/2. Feladat.py
@@ -1,6 +1,25 @@
+T=[0, 1, 2, 3, 4, 5, 6]
+def napok(napok):
+    for x in T:
+        if x==0:
+            Mennyi="Hétfő"
+        if x==1:
+            Mennyi="Kedd"
+        if x==2:
+            Mennyi="Szerda"
+        if x==3:
+            Mennyi="Csütörtök"
+        if x==4:
+            Mennyi="Péntek"
+        if x==5:
+            Mennyi="Szombat"
+        if x==6:
+            Mennyi="Vasárnap"
 
-a=int(input("hányas számú napon indultál "))
-b=int(input("hány napig voltál távol "))
-while b>7:
-    b=b-7
-print(b+a)
+Mikor=int(input("hányas számú napon indultál "))
+Mennyi=int(input("hány napig voltál távol "))
+while Mennyi>7:
+    Mennyi=Mennyi-7
+Mennyi=Mennyi+Mikor
+napok(6)
+print(Mennyi)

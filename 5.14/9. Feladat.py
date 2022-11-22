cfb83855130d98ad6b56b15303926156bc5a28f8
Szilágyi Zsolt <zsoltika20030@gmail.com>
2022-11-22 15:50:11 +0100

    Feladat

---
+++ 5.14/9. Feladat.py
@@ -0,0 +1,41 @@
+import turtle
+kép=turtle.Screen()
+def rajzolj_oszlopot(t, magassag):
+    t.begin_fill()
+    
+    t.left(90)
+    t.forward(magassag)
+    if magassag>0:    
+        t.write("  "+ str(magassag))
+    if magassag<0:
+        t.penup()
+        t.backward(15)
+        t.write(" "+str(magassag))
+        t.forward(15)
+        t.pendown()
+    t.right(90)
+    t.forward(40)
+    t.right(90)
+    t.forward(magassag)
+    t.left(90)
+    t.end_fill()
+    t.penup()
+    t.forward(10)
+    t.pendown()
+ablak = turtle.Screen()
+ablak.bgcolor("lightgreen")
+Eszti = turtle.Turtle()
+Eszti.color("blue", "red")
+Eszti.pensize(3)
+xs = [48,117,-200,240,-160,-260,220]
+for m in xs:
+    if m>200:
+        Eszti.color("blue", "red")
+    if m >=100 and m <=200:
+        Eszti.color("blue", "yellow")
+    if m<100:
+        Eszti.color("blue", "green")
+    rajzolj_oszlopot(Eszti, m)
+ablak.mainloop()
+
+#Ahol negatív a szám ott negatív irányban rajzolja meg

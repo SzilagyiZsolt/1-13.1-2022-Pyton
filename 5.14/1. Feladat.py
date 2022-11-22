e76ba43913450a90e447d017b43de06ca4f674b2
Szilágyi Zsolt <zsoltika20030@gmail.com>
2022-11-22 13:20:37 +0100

    Feladat

--- 5.14/1. Feladat.py
+++ 5.14/1. Feladat.py
@@ -1,10 +1,21 @@
-def napok(hétfő, kedd, szerda, csütörtök, péntek, szombat, vasárnap):
-    hétfő=0
-    kedd=1
-    szerda=2
-    csütörtök=3
-    péntek=4
-    szombat=5
-    vasárnap=6
-print(napok(0, 1, 2, 3, 4, 5, 6))
+T=[0, 1, 2, 3, 4, 5, 6]
+Mennyi=int(input("írj egy számot 0-tól 6-ig: "))
+def nap(Mennyi):
+    for x in T:
+        if Mennyi==0:
+            Mennyi="Hétfő"
+        elif Mennyi==1:
+            Mennyi="Kedd"
+        elif Mennyi==2:
+            Mennyi="Szerda"
+        elif Mennyi==3:
+            Mennyi="Csütörtök"
+        elif Mennyi==4:
+            Mennyi="Péntek"
+        elif Mennyi==5:
+            Mennyi="Szombat"
+        elif Mennyi==6:
+            Mennyi="Vasárnap"
+    print(Mennyi)
+nap(Mennyi)
 
--- 5.14/2. Feladat.py
+++ 5.14/2. Feladat.py
@@ -1,6 +1,22 @@
-
-a=int(input("hányas számú napon indultál "))
-b=int(input("hány napig voltál távol "))
-while b>7:
-    b=b-7
-print(b+a)
+T=[0, 1, 2, 3, 4, 5, 6]
+Mikor=int(input("hányas számú napon indultál? "))
+Mennyi=int(input("hány napig voltál távol? "))
+while Mennyi>7:
+    Mennyi=Mennyi-7
+Mennyi=Mennyi+Mikor
+for x in T:
+    if Mennyi==0:
+        Mennyi="Hétfő"
+    elif Mennyi==1:
+        Mennyi="Kedd"
+    elif Mennyi==2:
+        Mennyi="Szerda"
+    elif Mennyi==3:
+        Mennyi="Csütörtök"
+    elif Mennyi==4:
+        Mennyi="Péntek"
+    elif Mennyi==5:
+        Mennyi="Szombat"
+    elif Mennyi==6:
+        Mennyi="Vasárnap"
+print(f"Ezen a napon térsz haza: {Mennyi}")

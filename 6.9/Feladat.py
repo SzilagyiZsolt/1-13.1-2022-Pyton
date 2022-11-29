d699cef1a1b8063edc6f769d3857284b82d75177
Szilágyi Zsolt <zsoltika20030@gmail.com>
2022-11-29 11:46:15 +0100

    Feladat

--- 6.9/Feladat.py
+++ 6.9/Feladat.py
@@ -29,9 +29,11 @@ def fordulj_orajarasi_iranyba(n):
         print("D")
         return n
     elif n!="K" or "É" or "Ny" or "D":
-        teszt(fordulj_orajarasi_iranyba(n) == None)
+        teszt(fordulj_orajarasi_iranyba(42) == None)
+        return n
     elif n!="K" or "É" or "Ny" or "D":
         teszt(fordulj_orajarasi_iranyba(n) == None)
+        return n
 teszt(fordulj_orajarasi_iranyba(n) == n)
 
 
@@ -40,30 +42,38 @@ a=int(input("írj egy számot 0-tól 6-ig: "))
 def nap_nev(a):
     if a==0:
         a="Hétfő"
+        return a
     elif a==1:
         a="Kedd"
+        return a
     elif a==2:
         a="Szerda"
+        return a
     elif a==3:
         a="Csütörtök"
+        return a
     elif a==4:
         a="Péntek"
+        return a
     elif a==5:
         a="Szombat"
+        return a
     elif a==6:
         a="Vasárnap"
-
+        return a
 if a==0:
-    teszt(nap_nev(a) == a)
+    teszt(nap_nev(a) == "Hétfő")
 elif a==1:
-    teszt(nap_nev(a) == a)
+    teszt(nap_nev(a) == "Kedd")
 elif a==2:
-    teszt(nap_nev(a) == a)
+    teszt(nap_nev(a) == "Szerda")
 elif a==3:
-    teszt(nap_nev(a) == a)
+    teszt(nap_nev(a) == "Csütörtök")
 elif a==4:
-    teszt(nap_nev(a) == a)
+    teszt(nap_nev(a) == "Péntek")
 elif a==5:
-    teszt(nap_nev(a) == a)
+    teszt(nap_nev(a) == "Szombat")
 elif a==6:
-    teszt(nap_nev(a) == a)
+    teszt(nap_nev(a) == "Vasárnap")
+elif a>6:
+    teszt(nap_nev(a) == None)

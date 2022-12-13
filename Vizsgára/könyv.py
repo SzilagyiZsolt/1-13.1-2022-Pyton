1e52ddd001384a5ba5d15848cb9c6b1e487d561a
Szilágyi Zsolt <zsoltika20030@gmail.com>
2022-12-13 08:44:04 +0100

    Vizsgára

--- Vizsgára/könyv.py
+++ Vizsgára/könyv.py
@@ -6,8 +6,8 @@ def könyv(a):
 a=input("Add meg a könyv címét!")
 while(a!=""):
     b=int(input("Add meg az oldalak számát!"))
-    e=könyv(b)
-    if e:
+    c=könyv(b)
+    if c:
         print("A", a, "hosszú terjedelmű könyv")
     else:
         print("A", a, "rövid terjedelmű könyv")

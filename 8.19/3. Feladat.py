d560701974104f254f7ee4e6700be62f2e721d0f
Szilágyi Zsolt <zsoltika20030@gmail.com>
2022-12-13 10:30:31 +0100

    Vizsgára

--- 8.19/3. Feladat.py
+++ 8.19/3. Feladat.py
@@ -1,9 +1,9 @@
 gyumolcs = input("írj be egy gyümölcsöt!")
 darab = 0
 betu=input("Ird be a betűt amit számolni akarsz! ")
-def karakter_szamlalas(a, b):
+def karakter_szamlalas(gyumolcs, darab, betu):
     for karakter in gyumolcs:
         if karakter == betu:
-            darab=++1
+            darab += 1
     print(darab)
-karakter_szamlalas(gyumolcs, darab)
+karakter_szamlalas(gyumolcs, darab, betu)

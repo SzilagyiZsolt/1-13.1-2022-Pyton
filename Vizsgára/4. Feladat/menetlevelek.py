50beb82660ef1305e1487a76cc7ed7b235fe35cd
Szilágyi Zsolt <zsoltika20030@gmail.com>
2023-01-19 16:35:44 +0100

    Gyakorlás

--- Vizsgára/4. Feladat/menetlevelek.py
+++ Vizsgára/4. Feladat/menetlevelek.py
@@ -1,11 +1,11 @@
 from classes import Menetlevél
-r=open("fogyasztas.txt", "r")
+r=open("fogyasztas.txt", "r", encoding="utf-8")
 T=[]
 
 for line in r:
         a=line.split(";")
         b=Menetlevél(a[0])
-        b.setMEG(int(a[1]))
+        a.setMEG(int(a[1]))
         b.setFOGY(float(a[2][0:len(a[2])-2]))
         T.append(b)
 r.close()
